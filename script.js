b22572ca65896b025ea4132ed1d9ed6c866157f9
rijkvanzanten <rijkvanzanten@me.com>
2018-10-28 14:18:06 -0400

    Add colors

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ clipboard.on('success', function(event) {
   });
 });
 
-
 // Dynamically change the font size of the text-sizes example
 var textSizeInput = document.querySelector('#text-size-adjust input');
 var textSizeOutput = document.querySelector('#text-size-adjust output');
@@ -20,4 +19,15 @@ var textSizeOutput = document.querySelector('#text-size-adjust output');
 textSizeInput.addEventListener('input', function(event) {
   textSizeOutput.innerText = event.target.value;
   document.body.style.setProperty('--text-size-demo', event.target.value + 'px');
+});
+
+
+// Dynamically change the color of the logo
+var logoColorInputs = document.querySelectorAll('#logo-color-switcher input');
+logoColorInputs.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    var color = event.target.value;
+    var colorVar = 'var(--' + color + ')';
+    document.body.style.setProperty('--logo-fill', colorVar);
+  });
 });
