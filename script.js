04aae2d27fb77f06913f8a6905784b57156e01ff
rijkvanzanten <rijkvanzanten@me.com>
2018-10-28 12:39:27 -0400

    Add typography

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 new SmoothScroll('a[href*="#"]');
+
+var textSizeInput = document.querySelector("#text-size-adjust input");
+var textSizeOutput = document.querySelector("#text-size-adjust output");
+
+textSizeInput.addEventListener("input", function(event) {
+  textSizeOutput.innerText = event.target.value;
+  document.body.style.setProperty("--text-size-demo", event.target.value + "px");
+});
