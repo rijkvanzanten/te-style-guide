ea36ab47ba06f383a0a97cf704746c1f8d5a4938
rijkvanzanten <rijkvanzanten@me.com>
2018-10-28 13:02:38 -0400

    Copy hex function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+// Scroll smoothly to the different sections
 new SmoothScroll('a[href*="#"]');
 
-var textSizeInput = document.querySelector("#text-size-adjust input");
-var textSizeOutput = document.querySelector("#text-size-adjust output");
+// Activate the "click to copy" buttons
+var clipboard = new ClipboardJS('.button-copy');
+clipboard.on('success', function(event) {
+  var button = event.trigger;
+  button.innerText = "Copied!";
+  
+  button.addEventListener("mouseout", function(event) {
+    button.innerText = "Copy hex";
+  });
+});
 
-textSizeInput.addEventListener("input", function(event) {
+
+// Dynamically change the font size of the text-sizes example
+var textSizeInput = document.querySelector('#text-size-adjust input');
+var textSizeOutput = document.querySelector('#text-size-adjust output');
+
+textSizeInput.addEventListener('input', function(event) {
   textSizeOutput.innerText = event.target.value;
-  document.body.style.setProperty("--text-size-demo", event.target.value + "px");
+  document.body.style.setProperty('--text-size-demo', event.target.value + 'px');
 });
